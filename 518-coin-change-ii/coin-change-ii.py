44d5fb560d85be157e26c43aa2c932a2fde6b0b3
momulaharinathreddy741 <momulaharinathreddy74@gmail.com>
2026-09-05 18:26:13 +0530

    Time: 467 ms (44.15%) | Memory: 52.5 MB (46.09%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,18 +1,30 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        # n=len(coins)
+        # dp=[[-1]*(amount+1) for _ in range(n)]
+        # def solver(ind,tar):
+        #     if tar==0:
+        #         return 1
+        #     if ind==n:
+        #         return 0
+        #     if dp[ind][tar]!=-1:
+        #         return dp[ind][tar]
+        #     not_take=solver(ind+1,tar)
+        #     take=0
+        #     if coins[ind]<=tar:
+        #         take=solver(ind,tar-coins[ind])
+        #     dp[ind][tar]=take+not_take
+        #     return dp[ind][tar] 
+        # return solver(0,amount)
         n=len(coins)
-        dp=[[-1]*(amount+1) for _ in range(n)]
-        def solver(ind,tar):
-            if tar==0:
-                return 1
-            if ind==n:
-                return 0
-            if dp[ind][tar]!=-1:
-                return dp[ind][tar]
-            not_take=solver(ind+1,tar)
-            take=0
-            if coins[ind]<=tar:
-                take=solver(ind,tar-coins[ind])
-            dp[ind][tar]=take+not_take
-            return dp[ind][tar] 
-        return solver(0,amount)
+        dp=[[0]*(amount+1) for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][0]=1
+        for i in range(n-1,-1,-1):
+            for j in range(1,amount+1):
+                not_take=dp[i+1][j]
+                take=0
+                if coins[i]<=j:
+                    take=dp[i][j-coins[i]]
+                dp[i][j]=take+not_take
+        return dp[0][amount]
